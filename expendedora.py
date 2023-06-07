41b2042870ec533bd5d6fc3c35744a52a167db54
MarcVelasc <124969283+MarcVelasc@users.noreply.github.com>
2023-06-07 09:01:20 -0400

    Add files via upload

---
+++ expendedora.py
@@ -0,0 +1,97 @@
+#Instrucciones
+#El examen final requiere el siguiente software:
+
+#Se necesita realizar un software que simule la operacion de una maquina expendedora de cafe, esta maquina debe contar con un stock de tazas de cafe de 300 tazas de cafe o su equivalente en litros de cafe considerando que cada taza tiene 150 ml de Operacion, el cual el software debe ingresar un numero N de usuarios que deben ingresar en monto de 5 bs, que pueden ser en cambio de 2bs, 1 bs o moneda de 5 bs, debe mostrar mensaje que esta procesando el cafe y otro que simula la salida del cafe y debe realizar el siguiente reporte final de caja:
+
+#Cuantas tazas de cafe fueron vendidas en el dia.
+#Cuantos litros de cafe fueron vendidos y cuantos litros queda de saldo.
+#Cuanto es el monto en Bs de ganancia.
+#Cuantas monedas hay en la maquina de 1bs, 2 bs y 5 bs.
+#Condicionales de Aprobacion:
+
+#Debe incluir en su codigo funciones, condicionales, repetitivas y arreglos unidimensionales.
+#Deben realizarlo en Thonny o Pseint.
+tazas=300
+mlTotal=45000
+mlTaza=150
+preciocafe=5
+
+
+def dinerocaf5bs(dinero):
+    
+if dinerodecafe==1:
+    print("Te faltan 4bs")
+    dinerofaltante=input("Faltan 4bs: ")
+    if dinerofaltante==2:
+        print("Faltan 2bs")
+        dinerofaltante=int(input("Ingrese lo faltante: "))
+        if dinerofaltante==2:
+        print("Su cafe ya esta listo")
+elif dinerodecafe==2:
+    dinerofaltante=int(input("Faltan 3bs: "))
+    if dinerofaltante==2:
+        print("Falta 1bs")
+        dinerofaltante=int(input("Ingrese lo faltante: "))
+        if dinerofaltante==1:
+            print("su cafe ya esta listo")
+elif dinerodecafe==5:
+    print("su cafe ya esta listo")
+return
+
+personas=[]
+precioTotal=personas*preciocafe
+cafevendido=personas
+litrosdesaldo=(personas*mlTaza)-mlTotal
+print(".:EXPENDEDORA DE CAFE:.")
+print("Solo se puede igresar monedas de 1b, 2bs y 5bs")
+
+personas= int(input("¿Cuantos cafe necesita?: ")).append
+print("El precio total seria de ",precioTotal)
+dinerodecafe=input("El cafe esta a 5bs: ")
+
+print(f"cafe vendido,{cafevendido}")
+print(f"litros de saldo {litrosdesaldo}")
+num_usuarios=
+
+monedas_1bs=[]
+if monedas
+monedas_2bs=[]
+monedas_5bs=[]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
